c74cd0a39fc7c0ebd74227c2c75292a05ca10cdf
Willie Chalmers III <willieechalmers@gmail.com>
2017-10-01 19:10:21 -0500

    Finalize initialization

---
+++ src/com/codecentral/twotris/Twotris.java
@@ -0,0 +1,20 @@
+package com.codecentral.twotris;
+
+/**
+ * The primary entry point to 2tris.
+ *
+ * @author willieechalmers@gmail.com
+ * @version 0.1.0
+ * @since 10/1/17
+ */
+public class Twotris {
+
+    /**
+     * Starts a new instance of the game.
+     *
+     * @param args Command line arguments, currently ignored
+     */
+    public static void main(String[] args) {
+        // TODO: 10/1/17 Implement me
+    }
+}
