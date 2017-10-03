311f1b1977ec13ca1f968077a8d39bc69ed298b2
roblesda768 <32492394+roblesda768@users.noreply.github.com>
2017-10-03 12:17:09 -0500

    Add files via upload
    I added int x with a 0 value into the public class.

---
+++ Twotris.java
@@ -0,0 +1,21 @@
+package com.codecentral.twotris;
+
+/**
+ * The primary entry point to 2tris.
+ *
+ * @author willieechalmers@gmail.com
+ * @version 0.1.0
+ * @since 10/1/17
+ */
+public class Twotris {
+	int x = 0;
+
+    /**
+     * Starts a new instance of the game.
+     *
+     * @param args Command line arguments, currently ignored
+     */
+    public static void main(String[] args) {
+        // TODO: 10/1/17 Implement me
+    }
+}
