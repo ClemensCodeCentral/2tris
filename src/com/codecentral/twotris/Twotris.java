c031f96141f0558026fbe9f0a31b47bb96f0788e
Willie Chalmers III <willieechalmers@gmail.com>
2017-10-05 12:17:12 -0500

    Set up framework for game

--- src/com/codecentral/twotris/Twotris.java
+++ src/com/codecentral/twotris/Twotris.java
@@ -16,5 +16,65 @@ public class Twotris {
      */
     public static void main(String[] args) {
         // TODO: 10/1/17 Implement me
+       startNewGame();
+    }
+
+    public static void startNewGame() {
+    	new Twotris().startGame()
+    }
+
+    private Twotris() {
+    	// TODO: Initialize variables
+    }
+
+    public void startGame() {
+    	startGame(null);
+    }
+
+    public void startGame(String[] args) {
+    	if (args != null) {
+    		// Handle args
+    	}
+    }
+
+    public static class Player {
+    	private int points;
+    }
+
+    public static class Tetroid {
+
+    	/**
+    	 * A two-dimensional integer array representing color values
+    	 * A value of -1 indicates there should be no display for that component.
+    	 */
+    	private int[][] components;
+
+    	/**
+    	 * Creates a new Tetroid with the display
+    	 */
+    	public Tetroid(int[][] componentDefinition) {
+    		components = componentDefinition;
+    	}
+
+    	private void validateComponentDefinition(int[][] componentDefinition) {
+    		if (true) {
+    			// TODO: Make sure RGB values are valid and the component isn't larger than the game.
+    			return;
+    		}
+    		throw new IllegalArgumentException("Component definition");
+    	}
+
+		public void rotate(Direction direction) {
+			if (direction == Direction.CLOCKWISE) {
+				// TODO: Do something with components
+			} else if (direction == Direction.COUNTER_CLOCKWISE) {
+				// TODO: Do something with components
+			}
+		}
+
+    }
+
+    public enum Direction {
+    	CLOCKWISE, COUNTER_CLOCKWISE
     }
 }
